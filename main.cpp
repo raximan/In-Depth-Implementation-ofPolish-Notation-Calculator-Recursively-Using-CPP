748ba822f500c7b472343d0692e5f2afe062f15a
Raximan <96132332+raximan@users.noreply.github.com>
2024-01-19 20:24:42 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+#include "tree.hpp";
+#include "Token.hpp";
+
+
+
+
+
+double calculate(string inputstr);
+
+int main(){
+    string expression = "- * + 5 2 - 7 1 4";
+    calculate(expression);
+	return 1;
+}
+
+double calculate(string inputstr){
+    TokenList* tknlist=convertStr_to_TokenList(inputstr); //Convert String to TokenList, return head
+    SyntaxTree* syntaxt= parse(tknlist);                   //Convert tknlist to tree, return root
+    double calculationResult=syntaxt->pnode->getValue();  //Calculate from tree
+    syntaxt->print();                  // Print tree in conventional expression
+    cout<<" = "<<calculationResult<<"\n";    // Complete the conventional expression with calculation result
+    syntaxt->deleteTree();
+}
+
+
+
+
+
+
+
+
